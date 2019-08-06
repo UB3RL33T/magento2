22c6b7a90d6c805a82f54f1a3ad1348f7a94df60
Tobias Ritter <t.ritter@imi.de>
2019-08-06 17:48:25 +0200

    check if region is set in post
    if the `region` is not set php will throw an Undefined Index Exception notice

--- app/code/Magento/Customer/Block/Address/Edit.php
+++ app/code/Magento/Customer/Block/Address/Edit.php
@@ -120,7 +120,7 @@ class Edit extends \Magento\Directory\Block\Data
         if ($postedData = $this->_customerSession->getAddressFormData(true)) {
             $postedData['region'] = [
                 'region_id' => isset($postedData['region_id']) ? $postedData['region_id'] : null,
-                'region' => $postedData['region'],
+                'region' => isset($postedData['region']) ? $postedData['region'] : null,
             ];
             $this->dataObjectHelper->populateWithArray(
                 $this->_address,
